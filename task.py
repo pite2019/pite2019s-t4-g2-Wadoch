085b674cccd294a80afe3f0c165346d1c085de06
Wadoch <dwadoch@gmail.com>
2019-04-12 14:25:19 +0200

    first approach

--- task.py
+++ task.py
@@ -1,4 +1,3 @@
-#Write a library that contains a class that can represent any 2𝑥2 real matrice. 
 #Include two functions to calculate the sum and product of two matrices. 
 #Next, write a program that imports this library module and use it to perform calculations.
 #Examples:
@@ -23,3 +22,47 @@
 #
 #Delete these comments before commit!
 #Good luck.
+import math
+
+class Matrix:
+	def __init__(self, *val):
+		self.num = int(math.sqrt(val.__len__()))
+		self.arr = []
+
+		for i in range( self.num ):
+			temp_arr = []
+
+			for j in range( self.num ):
+				temp_arr.append(val[i*self.num + j])
+
+			self.arr.append(temp_arr)
+
+	def __str__(self):
+		return "{}".format(self.arr)
+
+	def add(self, matrix_2):
+		new_arr = []
+		for i in range( self.arr.__len__() ):
+			temp_arr = []
+			
+			for j in range( self.arr[0].__len__() ):
+				temp_arr.append( self.arr[i][j] + matrix_2.arr[i][j] )
+			new_arr.append(temp_arr)
+
+		return new_arr
+
+		
+
+def main():
+	matrix_1 = Matrix(4,5,6,7)
+	matrix_2 = Matrix(2,2,2,1)
+	matrix_4 = Matrix(1,1,1,2,2,2,3,3,3)
+
+	matrix_3 = matrix_2.add(matrix_1)
+
+	print(matrix_1)
+	print(matrix_2)
+	print(matrix_3)
+	print(matrix_4)
+if __name__ == "__main__":
+	main()
